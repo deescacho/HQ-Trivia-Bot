a3700e1bec346aba3ca05495808e5d832d83dd1f
deescacho <deescachosmail@gmail.com>
2018-07-29 12:58:29 -0400

    Some Changes to Main and first image

--- main.py
+++ main.py
@@ -3,4 +3,18 @@
 #Created the 28th of July, 2018
 #Last Modified: 28/07/2018 (DD/MM/YYY)
 
-print('Hello World')
+import os
+from PIL import Image
+from pytesseract import image_to_string
+
+dirname = os.path.dirname(__file__)
+fileOne = os.path.join(dirname, 'img/1.png')
+fileTwo = os.path.join(dirname, 'img/2.jpg')
+fileThree = os.path.join(dirname, 'img/3.jpg')
+
+myText = image_to_string(Image.open(fileOne))
+print(myText)
+myText = image_to_string(Image.open(fileTwo))
+print(myText)
+myText = image_to_string(Image.open(fileThree))
+print(myText)
